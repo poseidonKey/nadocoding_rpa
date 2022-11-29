677d052ad040e154ea19cf9b1d1f2941ddb90c7b
poseidonKey <poseidonKey@github.com>
2022-11-29 10:20:11 +0900

    keyboard 까지.

--- 2_desktop/8_keyboard.py
+++ 2_desktop/8_keyboard.py
@@ -1,3 +1,6 @@
+# https://automatetheboringstuff.com/2e/chapter20/
+#  사용하는 키보드 관련 내용은 위 사이트 참조. 중간 정도에 있음.
+
 import pyautogui
 w = pyautogui.getWindowsWithTitle("제목 없음")[0] # 메모장 1개 띄운 상태에서 가져옴
 w.activate()
@@ -26,6 +29,8 @@ w.activate()
 # Ctrl 누르고 > Alt 누르고 > Shift 누르고 > A 누르고 > A 떼고 > Shift 떼고 > Alt 떼고 > Ctrl 떼고
 # pyautogui.hotkey("ctrl", "a")
 
+
+# pip install pyperclip 한글의 사용을 위해 install
 import pyperclip
 # pyperclip.copy("나도코딩") # "나도코딩" 글자를 클립보드에 저장
 # pyautogui.hotkey("ctrl", "v") # 클립보드에 있는 내용을 붙여넣기
